1497f4a5a258fd6a1032c3b3aa6e4a09f938be8c
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-03-22 13:01:46 +0700

    added map activity and submit answer activity

---
+++ ITB-Race/app/src/main/java/com/example/chairuniaulianusapati/itb_race/SubmitAnswerActivity.java
@@ -0,0 +1,13 @@
+package com.example.chairuniaulianusapati.itb_race;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SubmitAnswerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_submit_answer);
+    }
+}

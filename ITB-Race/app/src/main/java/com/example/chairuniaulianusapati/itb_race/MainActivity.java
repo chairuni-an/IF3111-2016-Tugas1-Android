cea7bbb6c5caea0b72d0757c5f59a0e092748e9b
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-03-25 17:18:45 +0700

    Make Socket

--- ITB-Race/app/src/main/java/com/example/chairuniaulianusapati/itb_race/MainActivity.java
+++ ITB-Race/app/src/main/java/com/example/chairuniaulianusapati/itb_race/MainActivity.java
@@ -4,14 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView response;
+    Button buttonStart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //dari sini
+        buttonStart = (Button) findViewById(R.id.startButton);
+        response = (TextView) findViewById(R.id.responseTextView);
+
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                response.setText("");
+                Client myClient = new Client("167.205.34.132", 3111, response);
+                myClient.execute();
+
+            }
+        });
+
+        //sampai sini
     }
 
     /** Called when the user clicks the Start button */

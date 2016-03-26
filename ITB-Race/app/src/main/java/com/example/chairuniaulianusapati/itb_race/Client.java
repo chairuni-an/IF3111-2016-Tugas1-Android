99ca55d543833bf4bba1da603e6a745248cfa60f
Chairuni Aulia Nusapati <chairuni.aulia.nusapati@gmail.com>
2016-03-26 11:55:40 +0700

    Sent the request

--- ITB-Race/app/src/main/java/com/example/chairuniaulianusapati/itb_race/Client.java
+++ ITB-Race/app/src/main/java/com/example/chairuniaulianusapati/itb_race/Client.java
@@ -5,13 +5,22 @@ package com.example.chairuniaulianusapati.itb_race;
  */
 
 import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.charset.StandardCharsets;
 
 public class Client extends AsyncTask<Void, Void, Void> {
 
@@ -32,23 +41,28 @@ public class Client extends AsyncTask<Void, Void, Void> {
         Socket socket = null;
 
         try {
+            JSONObject json = new JSONObject();
+            json.put("com", "req_loc");
+            json.put("nim", "13513054");
+
             socket = new Socket(dstAddress, dstPort);
-
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream(
-                    1024);
-            byte[] buffer = new byte[1024];
-
-            int bytesRead;
-            InputStream inputStream = socket.getInputStream();
-
+            Log.i("Client", "Connecting...");
+            PrintWriter out = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())), true);
+            out.println(json.toString());
+            Log.i("Client", "Request Message: " + json.toString());
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            int c;
+            String responseString = "";
+            while((c=in.read())!=-1){
+                responseString+=(char)c;
+            }
+            Log.i("Client", "Response: " + responseString);
          /*
           * notice: inputStream.read() will block if no data return
           */
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
-                byteArrayOutputStream.write(buffer, 0, bytesRead);
-                response += byteArrayOutputStream.toString("UTF-8");
-            }
 
+        }catch(org.json.JSONException e){
+            // nothing
         } catch (UnknownHostException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
